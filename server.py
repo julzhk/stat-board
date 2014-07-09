39928f60bb8ddf162ae643f14e9ad923648664a1
James Doc <jamesdoc@gmail.com>
2014-07-09 13:16:26 +0100

    Save to MongoDB

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import tornado.web
 import requests
 import config
 
+from time import time
+from pymongo import Connection
 from tornado.options import define, options, parse_command_line
 
 define("port", default=8080, help="run on the given port", type=int)
@@ -19,6 +21,13 @@ class Collector(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self, filter):
 
+        con = Connection()
+        db = con.statboard
+        sm = db.socialmedia
+        
+        # for stat in sm.find():
+        #     sm.remove(stat)
+
         for instauser in config.instagram_users:
             r = requests.get(
                 'https://api.instagram.com/v1/users/%s/?client_id=%s'
@@ -26,9 +35,21 @@ class Collector(tornado.web.RequestHandler):
             )
             r = r.json()
 
-            print instauser['name'] + ' - ' + str(r['data']['counts']['followed_by'])
+            insert = {
+                'service': 'instagram',
+                'user_account': instauser['user'],
+                'datetime': int(time()),
+                'followers': r['data']['counts']['followed_by']
+            }
+
+            sm.insert(insert)
+
+        for stat in sm.find():
+            print stat
 
         self.render("templates/index.html")
+
+
 
 app = tornado.web.Application([
     (r'/', IndexHandler),
