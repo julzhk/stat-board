bb082d9ac49554ccd813bb3f335e31bf7ab80c23
James Doc <jamesdoc@gmail.com>
2014-07-10 16:06:26 +0100

    Adding in function for merging arrays (unfinished)

--- server.py
+++ server.py
@@ -86,6 +86,25 @@ def instagram_counts(filter=None):
 
     return True
 
+def map_elements_for_chart(instagram_users=None, data=None):
+    """
+    >>> map_elements_for_chart()
+    []
+    >>> map_elements_for_chart([{'name': 'V&A', 'user': 'vamuseum', 'id': '390861362'}], [{u'_id': '53be9c603090781ac49d95b8', u'followers': 17550, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405000800}])
+    [[1405000800, 17550]]
+    >>> map_elements_for_chart([{'name': 'V&A', 'user': 'vamuseum', 'id': '390861362'}, {'name': 'V&A Shop', 'user': 'v_and_a_shop', 'id': '390861456'}],[{u'_id': '53be9c603090781ac49d95b8', u'followers': 17550, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405000800}])
+    [[1405000800, 17550, None]]
+    >>> map_elements_for_chart([{'name': 'V&A', 'user': 'vamuseum', 'id': '390861362'}, {'name': 'V&A Shop', 'user': 'v_and_a_shop', 'id': '390861456'}],[{u'_id': '53be9c603090781ac49d95b8', u'followers': 17550, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405000800},{u'_id': '53bea23c3090781b672f229f', u'followers': 486, u'user_account': u'v_and_a_shop', u'service': u'instagram', u'datetime': 1405000800}])
+    [[1405000800, 17550, 486]]
+    >>> map_elements_for_chart([{'name': 'V&A', 'user': 'vamuseum', 'id': '390861362'}, {'name': 'V&A Shop', 'user': 'v_and_a_shop', 'id': '390861456'}],[{u'_id': '53be9c603090781ac49d95b8', u'followers': 17550, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405000800},{u'_id': '53bea23c3090781b672f229f', u'followers': 486, u'user_account': u'v_and_a_shop', u'service': u'instagram', u'datetime': 1405000800},{u'_id': '53bea4943090781b672f22a4', u'followers': 17552, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405002900},{u'_id': '53bea4943090781b672f22a5', u'followers': 486, u'user_account': u'v_and_a_shop', u'service': u'instagram', u'datetime': 1405002900}])
+    [[1405000800, 17550, 486], [1405002900, 17552, 486]]
+    >>> map_elements_for_chart([{'name': 'V&A', 'user': 'vamuseum', 'id': '390861362'}, {'name': 'V&A Shop', 'user': 'v_and_a_shop', 'id': '390861456'}],[{u'_id': '53be9c603090781ac49d95b8', u'followers': 17550, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405000800},{u'_id': '53bea23c3090781b672f229f', u'followers': 486, u'user_account': u'v_and_a_shop', u'service': u'instagram', u'datetime': 1405000800},{u'_id': '53bea4943090781b672f22a4', u'followers': 17552, u'user_account': u'vamuseum', u'service': u'instagram', u'datetime': 1405002900},{u'_id': '53bea4943090781b672f22a5', u'followers': 486, u'user_account': u'v_and_a_shop', u'service': u'instagram', u'datetime': 1405002910}])
+    [[1405000800, 17550, 486], [1405002900, 17552, None],[1405002910, None, 486]]
+    """
+
+    return [[1405000800, 17550]]
+
+
 class Collector(tornado.web.RequestHandler):
     def get(self, filter):
 
