492b826aa26c4b8c462c0caa9fa7db2af5b4fefa
julzhk <jharley@vam.ac.uk>
2014-07-23 10:02:42 +0100

    config is no longer sensitive

---
+++ config.py
@@ -0,0 +1,36 @@
+instagram_users = [
+    {
+        'name'  : 'V&A',
+        'user'  : 'vamuseum',
+        'id'    : '390861362'
+    },
+    {
+        'name'  : 'V&A Shop',
+        'user'  : 'v_and_a_shop',
+        'id'    : '589901649'
+    },
+    {
+        'name'  : 'V&A Dundee',
+        'user'  : 'vadundee',
+        'id'    : '223144499'
+    },
+]
+
+twitter_users = [
+    {
+        'name'  : 'V&A',
+        'user'  : 'V_and_A'
+    },
+    {
+        'name'  : 'V&A Shop',
+        'user'  : 'V_and_A_Shop'
+    },
+    {
+        'name'  : 'V&A Dundee',
+        'user'  : 'VADundee'
+    },
+    {
+        'name'  : 'Museum of Childhood',
+        'user'  : 'MuseumChildhood'
+    },
+]
